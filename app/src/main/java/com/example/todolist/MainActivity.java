c9c78042b6b0f2569954f2d4963c9eae73bf0097
Shan Fuyang <fuyang@Shans-MacBook-Pro.local>
2019-09-21 01:24:19 -0700

    viewHolder pattern to be more performance

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.todolist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+
+    private StringAdapter mAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main); //link to the xml file in the layout
+        //link to the xml file in the layout
+        setContentView(R.layout.activity_main);
+
+
+        //set up adapter
+
+        mAdapter = new StringAdapter(this, R.layout.list_item, R.id.list_item_textView);
+
+        ListView listView = (ListView) findViewById(R.id.activity_main_listview);
+        listView.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/com/example/todolist/StringAdapter.java
+++ app/src/main/java/com/example/todolist/StringAdapter.java
@@ -1,4 +1,70 @@
 package com.example.todolist;
 
-public class StringAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringAdapter extends ArrayAdapter<String> {
+
+    private int mResource;
+    private int mTextViewResource;
+
+    private LayoutInflater mLayoutInflater;
+    private List<String> mItems = new ArrayList<String>();
+
+    public StringAdapter(Context context, int resource, int textViewResource) {
+        super(context, resource, textViewResource);
+
+        mResource = resource;
+        mTextViewResource = textViewResource;
+
+        mLayoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        mItems.add("Fuyang");
+        mItems.add("Panda");
+        mItems.add("Cub");
+        mItems.add("Ethan");
+    }
+
+    //how many items in it
+    @Override
+    public int getCount() {
+        return mItems.size();
+    }
+
+
+    //how is the item looks like
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(mResource, null);
+            viewHolder = new ViewHolder();
+            viewHolder.itemTextView = (TextView) convertView.findViewById(mTextViewResource);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        final String item = mItems.get(position);
+        viewHolder.itemTextView.setText(item);
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView itemTextView;
+
+    }
 }
